32eaec88ad716d9889b359440a89788913e55069
jaspreet <jaspreetjhass91@gmail.com>
2021-06-23 20:50:08 +0530

    initial-commit

---
+++ src/main/java/org/jp/micro/framework/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.jp.micro.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
